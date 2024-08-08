a110bd7a15d40e14c6ec1739804c3819438ec213
BennettM7 <ryanben@ad.unc.edu>
2024-08-08 14:37:07 -0400

    bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 import Home from './pages/home'
 import AboutMe from './pages/about-me';
 import Navbar from './components/navbar';
@@ -9,13 +9,13 @@ import Projects from './pages/projects';
 
 function App() {
     return (
-        <Router>
+        <Router basename="/portfolio-website">
             <Navbar />
-            <Routes basename="/portfolio-website">
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/about-me" element={<AboutMe />} />
-                <Route exact path="/courses" element={<Courses />} />
-                <Route exact path="/projects" element={<Projects /> } />
+            <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/about-me" element={<AboutMe />} />
+                    <Route exact path="/courses" element={<Courses />} />
+                    <Route exact path="/projects" element={<Projects /> } />
             </Routes>
         </Router>
     )
