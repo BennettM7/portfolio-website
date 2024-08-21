02888bfafcd5552fe4dc2147bcc2504e1e892dd3
Bennett Mangum <ryanben@ad.unc.edu>
2024-08-21 17:55:23 -0400

    Add buttons to home page to download Resume, link to linkedin and link to github.

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import headshot from '../photos/headshot.jpg';
+import Typography from '@mui/material/Typography';
 import '../App.css';
 import '../styles/about-me.css';
 
@@ -9,25 +10,25 @@ export default function AboutMe() {
             <div className='about-container'>
                 <div>
                     <h1>About Me</h1>
-                    <p>
+                    <Typography>
                         Hello World! I'm Bennett Mangum, a senior at the University of North Carolina at Chapel Hill, where I'm double majoring in Computer Science and Philosophy. Ever since I was a kid, I've been fascinated by computers, and I feel incredibly lucky to study what I love at such a great university.
-                    </p>
+                    </Typography>
                     
-                    <p>
+                    <Typography>
                         I'm aiming for a career in Software Engineering and have built a solid foundation in this field. I enjoy working in teams, where I can often be found taking on leadership roles to help guide our projects. At the same time, I understand the importance of listening and letting others take charge when needed. Inclusivity is key for me, and I always strive to ensure everyone feels heard and on the same page.
-                    </p>
+                    </Typography>
                     
-                    <p>
+                    <Typography>
                         My background in Philosophy has sharpened my problem-solving and critical thinking skills. I'm adept at analyzing different solutions and finding the best one that benefits everyone involved. This skill set helps me tackle challenges effectively and efficiently.
-                    </p>
+                    </Typography>
                     
-                    <p>
+                    <Typography>
                         If you're curious about what I'm working on or the classes I'm taking, check out the other tabs on my website. You'll also find links to my GitHub, LinkedIn, and resume below, where you can see more about my projects and experiences.
-                    </p>
+                    </Typography>
                     
-                    <p>
+                    <Typography>
                         I'd love to connect and share more about my journey!
-                    </p>
+                    </Typography>
                 </div>
             </div>
             <div className='headshot'>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,15 @@
 import HomePagePhoto from "../photos/Bennett_Mangums_Portfolio.png"
+import Button from "@mui/material/Button";
+import Resume from "../photos/Resume.pdf"
 import "../styles/home.css"
 export default function Home() {
+    const onDownload = () => {
+        const link = document.createElement("a");
+        link.download = 'Resume.pdf';
+        link.href = Resume;
+        link.click();
+      };
+
     return (
         <div className="title-container">
             <div className="welcome-container">
@@ -14,6 +23,42 @@ export default function Home() {
                             that I have taken at the University of North Carolina at Chapel Hill. Click on any of the tabs about to find more information! Below you can click
                             to my socials and get in contact with me through email as well!
                         </p>
+                    </div>
+                    <div className="hp-button-container">
+                        <Button 
+                            onClick={onDownload} 
+                            sx={{
+                                backgroundColor: '#E88D67', 
+                                color: 'white',
+                                width: 150,
+                            }}
+                            >
+                            Resume
+                        </Button>
+                        <Button
+                            href='https://www.linkedin.com/in/bennett-mangum-14824628a/'
+                            target="_blank"
+                            rel="noopener noreferrer" 
+                            sx={{
+                                backgroundColor: '#E88D67', 
+                                color: 'white',
+                                width: 150,
+                            }}
+                        >
+                                LinkedIn
+                        </Button>
+                        <Button
+                            href='https://github.com/BennettM7'
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            sx={{
+                                backgroundColor: '#E88D67', 
+                                color: 'white',
+                                width: 150,
+                            }}
+                        >
+                            GitHub
+                        </Button>
                     </div>
                 </div>
                 <img src={HomePagePhoto} alt="Home Page Photo" height={768} width={576} />
